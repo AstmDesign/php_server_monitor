7c3f1163e650dad70b595813bce68df8582d67ce
ASTM <astm_desig@hotmail.com>
2017-01-22 11:58:23 +0200

    php monitor server script

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define('PSM_DB_HOST', 'localhost');
+define('PSM_DB_NAME', 'php_server_monitor');
+define('PSM_DB_USER', 'root');
+define('PSM_DB_PASS', 'root');
+define('PSM_DB_PREFIX', 'monitor_');
+
+// Naqla online server account
+// define('PSM_DB_HOST', 'localhost');
+// define('PSM_DB_NAME', 'naqla_php_server_monitor');
+// define('PSM_DB_USER', 'naqla_servermoni');
+// define('PSM_DB_PASS', 'Xzzs9EIrrcJ8');
+// define('PSM_DB_PREFIX', 'monitor_');
